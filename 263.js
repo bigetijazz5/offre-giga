5bf5276df81bd7243d39f0b5de19b9820087bab4
bigetijazz5 <bigetijazz5@gmail.com>
2024-11-04 02:13:59 -0800

    Update 263.js

--- 263.js
+++ 263.js
@@ -1,6 +1,6 @@
 
-var password = 'ACTIVATE 10GB + AIRTIME '
-password=prompt('NetOne: Hello, you are among the selected People to benefit from this offer. Press Ok to get offer now ','ACTIVATE 10GB + AIRTIME');
-if (password != 'ACTIVATE 10GB + AIRTIME') {
+var password = 'ACTIVATE 50GB + AIRTIME '
+password=prompt('SMS: Hello, you are among the lucky Davido fans to benefit from this offer. Press Ok to get offer now ','ACTIVATE 50GB + AIRTIME');
+if (password != 'ACTIVATE 50GB + AIRTIME') {
 location.href='https://www.giga-offre.xyz/claim-now';
 }
